e79ee85d56a7eb44fc334c3bcac7a689d0cdfab7
oliceglad <vycheslavofficial@gmail.com>
2023-03-28 18:33:47 +0400

    added three screens and tab navigation

---
+++ spygame/components/screens/Online.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+function OnlineScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Online</Text>
+        </View>
+    );
+}
+
+export default OnlineScreen;
---
+++ spygame/components/screens/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text} from 'react-native'
+
+function ProfileScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>
+                Профиль
+            </Text>
+        </View>
+    )
+}
+
+export default ProfileScreen;
