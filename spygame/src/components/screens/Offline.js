6bb3a4d0ca6c2d1e05db4dfedac2c942e22de483
oliceglad <vycheslavofficial@gmail.com>
2023-03-30 22:38:50 +0400

    added JSON file and changed Offline screen

--- spygame/src/components/screens/Offline.js
+++ spygame/src/components/screens/Offline.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { View, Text } from 'react-native';
+import data from '../../../assets/data/data.json'
 
 function OfflineScreen() {
+
+    const locations = data.locations;
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Оффлайн-игра</Text>
+            <View>
+                {locations.map(location =>
+                    <View>
+                        <Text>
+                            {location.name}
+                        </Text>
+                        <Text>
+                            {location.roles}
+                        </Text>
+                    </View>)}
+            </View>
         </View>
     );
 }
