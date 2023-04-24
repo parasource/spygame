a1c911200b1d13754c5174004e2d87b02f34b762
oliceglad <vycheslavofficial@gmail.com>
2023-04-24 19:36:55 +0400

    deleted duplicate NavigationContainer

--- spygame/src/AppNavigation.js
+++ spygame/src/AppNavigation.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import HomeTabs from './HomeTabs';
 import EmptyPage from './components/pages/EmptyPage';
 
@@ -8,11 +8,9 @@ const Stack = createNativeStackNavigator()
 
 export default function AppNavigation() {
     return (
-        <NavigationContainer independent={true}>
-            <Stack.Navigator>
-                <Stack.Screen name='Home' component={HomeTabs}/>
-                <Stack.Screen name='Empty' component={EmptyPage}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name='Home' component={HomeTabs} />
+            <Stack.Screen name='Empty' component={EmptyPage} />
+        </Stack.Navigator>
     );
 }
