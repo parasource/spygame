121fa9b3211b1e7b52c0df13610dfc5730fbc331
oliceglad <vycheslavofficial@gmail.com>
2023-04-24 19:37:18 +0400

    deleted import NavigationContainer

--- spygame/src/AppNavigation.js
+++ spygame/src/AppNavigation.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native'
 import HomeTabs from './HomeTabs';
 import EmptyPage from './components/pages/EmptyPage';
 
