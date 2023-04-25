64831194add8aea40e384054beafdc1550b32a27
oliceglad <vycheslavofficial@gmail.com>
2023-04-25 18:25:42 +0400

    added exitFromOfflineGame function

--- spygame/src/components/pages/EmptyPage.js
+++ spygame/src/components/pages/EmptyPage.js
@@ -1,13 +1,34 @@
 import React from "react";
-import {View} from 'react-native'
+import { View, Alert } from 'react-native'
 import { AppButton } from "../UI/AppButton";
 
 export default function EmptyPage({ navigation }) {
+
+    const exitFromGame = () => {
+        Alert.alert(
+            'Выход из игры',
+            'Вы уверены?',
+            [
+                {
+                    text: 'Да',
+                    style: 'destructive',
+                    onPress: () => {
+                        navigation.navigate('Home')
+                    }
+                },
+                {
+                    text: 'Нет',
+                    style: 'cancel'
+                }
+            ]
+        )
+    }
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <AppButton
                 pressHandler={
-                    () => navigation.navigate('Home')
+                    () => exitFromGame()
                 }>
                 Назад
             </AppButton>
